a91d26c8bb4486c9db0257970912afe9fb7540ad
Yangdi Lu <31164339+MacLLL@users.noreply.github.com>
2023-07-12 16:40:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -48,3 +48,41 @@ ax.set_zlim(0, voxel_grid.shape[2])
 
 # Show the plot
 plt.show()
+
+
+
+
+import numpy as np
+
+def point_cloud_to_voxel(point_cloud, voxel_size, grid_shape):
+    # Compute the minimum and maximum coordinates of the point cloud
+    min_bound = np.min(point_cloud, axis=0)
+    max_bound = np.max(point_cloud, axis=0)
+    
+    # Compute the voxel grid dimensions
+    grid_size = max_bound - min_bound
+    voxel_grid = np.zeros(grid_shape, dtype=bool)
+    
+    # Calculate the voxel indices for each point in the point cloud
+    indices = ((point_cloud - min_bound) / voxel_size).astype(int)
+    
+    # Set the corresponding voxel grid values to True
+    voxel_grid[tuple(indices.T)] = True
+    
+    return voxel_grid
+
+# Sample point cloud data
+point_cloud = np.array([[1.2, 3.4, 2.1],
+                       [0.5, 2.3, 1.7],
+                       [1.8, 1.2, 0.6],
+                       [0.9, 3.6, 2.8]])
+
+# Voxel parameters
+voxel_size = 0.1
+grid_shape = (10, 10, 10)
+
+# Convert point cloud to voxel grid
+voxel_grid = point_cloud_to_voxel(point_cloud, voxel_size, grid_shape)
+
+# Print the voxel grid
+print(voxel_grid)
